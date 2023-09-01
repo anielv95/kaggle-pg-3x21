dab8948c3f18ceab9710d2de8307fcd5ad6eb0e5
aniel <aniel.villegas.mx@gmail.com>
2023-09-01 06:49:18 +0000

    exploring data

--- exploring_data.py
+++ exploring_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import zipfile
+from pathlib import Path
 
 with zipfile.ZipFile("/gh/kaggle-pg-3x21/data/playground-series-s3e21.zip") as z:
    # open the csv file in the dataset
@@ -9,3 +10,10 @@ with zipfile.ZipFile("/gh/kaggle-pg-3x21/data/playground-series-s3e21.zip") as z
 train.to_csv("/gh/kaggle-pg-3x21/sample.csv",index=False)
 
 # kaggle competitions submit -c playground-series-s3e21 -f ./data/sample.csv -m "Message"
+
+train = pd.read_csv("/gh/kaggle-pg-3x21/data/sample.csv")
+train.head(2)
+train.shape
+train.info()
+
+train.describe()
